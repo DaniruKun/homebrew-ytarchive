53eb8b72061ba4b1cd5c5f19b2e43b8d985b5167
Daniils Petrovs <daniils@platogo.com>
2023-07-21 20:54:21 +0200

    Update version

--- Formula/ytarchive.rb
+++ Formula/ytarchive.rb
@@ -1,21 +1,21 @@
 class Ytarchive < Formula
-  desc "Archive live and upcoming Youtube.com live streams"
-  homepage "https://github.com/Kethsar/ytarchive"
-  url "https://github.com/Kethsar/ytarchive/archive/refs/tags/latest.tar.gz"
-  version "0.3.1"
-  sha256 "f61ac9b3606476306c208dd87961a3ca846efa92eaad651fa0270293c13a54c2"
-  license "MIT"
-  head "https://github.com/Kethsar/ytarchive.git", branch: "master"
+  desc 'Archive live and upcoming Youtube.com live streams'
+  homepage 'https://github.com/Kethsar/ytarchive'
+  url 'https://github.com/Kethsar/ytarchive/archive/refs/tags/latest.tar.gz'
+  version '0.3.2'
+  sha256 '8075ddb116f9a7e861fd7b318dfce3277dbebbdc8a8c2fcdf8f025d98c9a26a8'
+  license 'MIT'
+  head 'https://github.com/Kethsar/ytarchive.git', branch: 'dev'
 
-  depends_on "go" => :build
-  depends_on "ffmpeg"
+  depends_on 'go' => :build
+  depends_on 'ffmpeg'
 
   def install
-    ENV["CGO_ENABLED"] = "0"
-    system "go", "build", *std_go_args(ldflags: "-s -w")
+    ENV['CGO_ENABLED'] = '0'
+    system 'go', 'build', *std_go_args(ldflags: '-s -w')
   end
 
   test do
-    system bin/"ytarchive", "--version"
+    system bin / 'ytarchive', '--version'
   end
 end
