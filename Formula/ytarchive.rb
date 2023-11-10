4d890ec016106e4dfc65d9b92f169cc607a8e52d
omerien <53191414+omerien@users.noreply.github.com>
2023-11-10 22:31:49 +0100

    Update ytarchive.rb

--- Formula/ytarchive.rb
+++ Formula/ytarchive.rb
@@ -3,7 +3,7 @@ class Ytarchive < Formula
   homepage 'https://github.com/Kethsar/ytarchive'
   url 'https://github.com/Kethsar/ytarchive/archive/refs/tags/latest.tar.gz'
   version '0.3.2'
-  sha256 '055d3e7129c737105d28f743bd3ce02fef57b116f95a8f3601a25c163634a252'
+  sha256 '14923e93948a34f55ffaf74926862b2eeced5db7c0c7f872e38097d0ad168b77'
   license 'MIT'
   head 'https://github.com/Kethsar/ytarchive.git', branch: 'dev'
 
